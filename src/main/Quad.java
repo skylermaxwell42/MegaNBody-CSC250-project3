2d14f5282403989afd2c8eb61f8424724bbcffdc
Skyler Maxwell <skylermaxwell42@gmail.com>
2017-11-02 13:28:43 -0400

    Initial file uploads

---
+++ src/main/Quad.java
@@ -0,0 +1,39 @@
+public class Quad {
+    
+    // create a new square with the given parameters (assume it is square)
+    public Quad(double xmid, double ymid, double length) {
+    }
+
+    // return the length of one side of the square quadrant
+    public double length() {
+    }
+
+    // does quadrant contain (x, y)?
+    public boolean contains(double x, double y) {
+    }
+
+    // return a new object that represents the northwest quadrant
+    public Quad NW() {
+    }
+
+    // return a new object that represents the northeast quadrant
+    public Quad NE() {
+    }
+
+    // return a new object that represents the southwest quadrant
+    public Quad SW() {
+    }
+
+    // return a new object that represents the southeast quadrant
+    public Quad SE() {
+    }
+
+    // draw an unfilled rectangle that represents the quadrant
+    public void draw() {
+    }
+
+    // return a string representation of the quadrant for debugging
+    public String toString() {
+    }
+
+}
